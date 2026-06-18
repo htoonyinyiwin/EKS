dc8860215a97150c051af9571fa053c48e229963
Htoo Nyi Nyi Win <htoonyinyi.w@7peakssoftware.com>
2026-06-18 21:43:30 +0700

    fix: remove unused redis replication group variables (tflint)

--- main-platform/redis-inputs.tf
+++ main-platform/redis-inputs.tf
@@ -28,72 +28,7 @@ variable "redis_port" {
   type        = number
 }
 
-variable "redis_replication_group" {
-  description = "Set to 1 to create an ElastiCache Replication Group, 0 to create a single node cluster"
-  type        = number
-}
-
 variable "redis_single_node_cluster" {
   description = "Set to 1 to create a single node Redis cluster, 0 to not create it"
   type        = number
-}
-
-variable "multi_az_enabled" {
-  description = "Whether to enable Multi-AZ for the Redis replication group"
-  type        = bool
-}
-
-variable "automatic_failover_enabled" {
-  description = "Whether to enable automatic failover for the Redis replication group"
-  type        = bool
-}
-
-variable "num_node_groups" {
-  description = "Number of node groups (shards) for the Redis replication group"
-  type        = number
-}
-
-variable "replicas_per_node_group" {
-  description = "Number of replicas per node group (shard) for the Redis replication group"
-  type        = number
-}
-
-variable "num_cache_clusters" {
-  description = "Number of cache clusters (nodes) in the replication group"
-  type        = number
-}
-
-variable "redis_engine" {
-  description = "Redis engine version (e.g., 7.0)"
-  type        = string
-}
-
-variable "redis_at_rest_encryption_enabled" {
-  description = "Enable at-rest encryption for the Redis cluster"
-  type        = bool
-}
-
-variable "redis_transit_encryption_enabled" {
-  description = "Enable in-transit encryption for the Redis cluster"
-  type        = bool
-}
-
-variable "redis_snapshot_retention_limit" {
-  description = "Number of days to retain automatic snapshots"
-  type        = number
-}
-
-variable "redis_snapshot_window" {
-  description = "Daily time range (in UTC) during which ElastiCache begins taking a daily snapshot of your cluster"
-  type        = string
-}
-
-variable "redis_maintenance_window" {
-  description = "Weekly time range (in UTC) during which maintenance on the cluster is performed"
-  type        = string
-}
-
-variable "redis_auto_minor_version_upgrade" {
-  description = "Whether to enable automatic minor version upgrades for the Redis cluster"
-  type        = bool
 }
