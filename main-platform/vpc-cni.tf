d144a203b3418523e9382bf4c0597551acee7caf
Htoo Nyi Nyi Win <htoonyinyi.w@7peakssoftware.com>
2026-06-19 00:52:47 +0700

    feat: add RBAC, NetworkPolicy, and pod securityContext

---
+++ main-platform/vpc-cni.tf
@@ -0,0 +1,11 @@
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "vpc-cni"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  configuration_values = jsonencode({
+    enableNetworkPolicy = "true"
+  })
+
+  depends_on = [module.eks]
+}
